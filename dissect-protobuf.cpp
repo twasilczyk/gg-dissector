7f7fdf3c797d7d6beb90d90f878b8bc45c7e46c3
Tomasz Wasilczyk <twasilczyk@pidgin.im>
2014-11-03 14:36:54 +0100

    Dissect varint and fixedint

--- dissect-protobuf.cpp
+++ dissect-protobuf.cpp
@@ -8,6 +8,10 @@
 
 static GGPFieldBlob protobuf_blob
 	("unknown string", "gg.protobuf_unknown_string", NULL);
+static GGPFieldBlob protobuf_varint
+	("unknown varint", "gg.protobuf_unknown_varint", NULL);
+static GGPFieldUINT32 protobuf_fixedint
+	("unknown fixedint", "gg.protobuf_unknown_fixedint", NULL);
 
 class ProtobufDissector
 {
@@ -27,6 +31,26 @@ public:
 		return (tvb_length_remaining(tvb, offset) > 0);
 	}
 
+	void dissect_fixedint(int id)
+	{
+		guint32 val = tvb_get_letohl(tvb, offset);
+
+		proto_tree_add_uint_format(tree, protobuf_fixedint, tvb, offset, 4, val,
+			"unknown field %d: %d", id, val);
+
+		offset += 4;
+	}
+
+	void dissect_varint(int id)
+	{
+		guint8 val = tvb_get_guint8(tvb, offset); //TODO: varint
+
+		proto_tree_add_bytes_format(tree, protobuf_varint, tvb, offset, 1, NULL,
+			"unknown field %d: %d", id, val);
+
+		offset += 1;
+	}
+
 	void dissect_str(int id)
 	{
 		guint8 length = tvb_get_guint8(tvb, offset++); //TODO: varint
@@ -34,7 +58,7 @@ public:
 		DISSECTOR_ASSERT(tvb_length_remaining(tvb, offset) >= length);
 
 		proto_tree_add_bytes_format(tree, protobuf_blob, tvb, offset, length, NULL,
-			"unknown string (%d)", id);
+			"unknown field %d: string[%d]", id, length);
 
 		offset += length;
 	}
@@ -45,8 +69,12 @@ public:
 		guint8 id = type >> 3;
 		type &= 7;
 
-		if (type == 2)
+		if (type == 0)
+			dissect_varint(id);
+		else if (type == 2)
 			dissect_str(id);
+		else if (type == 5)
+			dissect_fixedint(id);
 		else
 			DISSECTOR_ASSERT_NOT_REACHED();
 	}

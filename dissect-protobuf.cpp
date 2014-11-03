75fa8ba3ce7a2b8ed3b88d4e8f2ed2c6b02548ee
Tomasz Wasilczyk <twasilczyk@pidgin.im>
2014-11-03 17:41:22 +0100

    Implement and use PBDisplay

--- dissect-protobuf.cpp
+++ dissect-protobuf.cpp
@@ -6,12 +6,87 @@
 
 #include "ggp-field.hpp"
 
+using namespace std;
+
 static GGPFieldBlob protobuf_blob
-	("unknown string", "gg.protobuf_unknown_string", NULL);
+	("unknown string", "gg.unknown_string", NULL);
 static GGPFieldBlob protobuf_varint
-	("unknown varint", "gg.protobuf_unknown_varint", NULL);
+	("unknown varint", "gg.unknown_varint", NULL);
 static GGPFieldUINT32 protobuf_fixedint
-	("unknown fixedint", "gg.protobuf_unknown_fixedint", NULL);
+	("unknown fixedint", "gg.unknown_fixedint", NULL);
+
+static gint64
+parse_varint(tvbuff_t *tvb, int &&offset = 0)
+{
+	gint64 val = 0;
+	int val_len = 0;
+	int shift = 0;
+	bool more = true;
+
+	while (more) {
+		DISSECTOR_ASSERT(tvb_length_remaining(tvb, offset) > 0);
+		DISSECTOR_ASSERT(++val_len <= 8);
+
+		gint64 b = tvb_get_guint8(tvb, offset++);
+
+		more = b & 0x80;
+		b &= ~0x80;
+
+		b <<= shift;
+		shift += 7;
+
+		val |= b;
+	}
+
+	return val;
+}
+
+
+PBDisplay::PBDisplay(int expected_type):expected_type(expected_type)
+{
+}
+
+void PBDisplay::display(proto_tree *tree, tvbuff_t *tvb, int id, guint8 type)
+{
+	DISSECTOR_ASSERT(type == expected_type);
+
+	this->display(tree, tvb, id);
+}
+
+void PBDisplay::display(proto_tree *tree, tvbuff_t *tvb, int /* id */)
+{
+	this->display(tree, tvb);
+}
+
+void PBDisplay::display(proto_tree*, tvbuff_t*)
+{
+	DISSECTOR_ASSERT_NOT_REACHED();
+}
+
+PBDisplayFixedint::PBDisplayFixedint():PBDisplay(5) {}
+void PBDisplayFixedint::display(proto_tree *tree, tvbuff_t *tvb, int id)
+{
+	guint32 val = tvb_get_letohl(tvb, 0);
+	proto_tree_add_uint_format(tree, protobuf_fixedint, tvb, 0, 4, val,
+		"unknown field %d: %d", id, val);
+}
+
+PBDisplayVarint::PBDisplayVarint():PBDisplay(0) {}
+void PBDisplayVarint::display(proto_tree *tree, tvbuff_t *tvb, int id)
+{
+	gint val = parse_varint(tvb, 0);
+
+	proto_tree_add_bytes_format(tree, protobuf_varint, tvb,
+		0, tvb_length(tvb), NULL,
+		"unknown field %d: %d", id, val);
+}
+
+PBDisplayBlob::PBDisplayBlob():PBDisplay(2) {}
+void PBDisplayBlob::display(proto_tree *tree, tvbuff_t *tvb, int id)
+{
+	proto_tree_add_bytes_format(tree, protobuf_blob, tvb, 0, tvb_length(tvb), NULL,
+		"unknown field %d: string[%d]", id, tvb_length(tvb));
+}
 
 class ProtobufDissector
 {
@@ -19,32 +94,6 @@ private:
 	gint offset = 0;
 	tvbuff_t *tvb;
 	proto_tree *tree;
-
-	gint64
-	parse_varint()
-	{
-		gint64 val = 0;
-		int val_len = 0;
-		int shift = 0;
-		bool more = true;
-
-		while (more) {
-			DISSECTOR_ASSERT(tvb_length_remaining(tvb, offset) > 0);
-			DISSECTOR_ASSERT(++val_len <= 8);
-
-			gint64 b = tvb_get_guint8(tvb, offset++);
-
-			more = b & 0x80;
-			b &= ~0x80;
-
-			b <<= shift;
-			shift += 7;
-
-			val |= b;
-		}
-
-		return val;
-	}
 
 public:
 	ProtobufDissector(tvbuff_t *tvb, proto_tree *tree):
@@ -59,39 +108,43 @@ public:
 
 	void dissect_fixedint(int id)
 	{
-		guint32 val = tvb_get_letohl(tvb, offset);
+		DISSECTOR_ASSERT(tvb_length_remaining(tvb, offset) >= 4);
 
-		proto_tree_add_uint_format(tree, protobuf_fixedint, tvb, offset, 4, val,
-			"unknown field %d: %d", id, val);
-
+		tvbuff_t *sub_tvb = tvb_new_subset_length(tvb, offset, 4);
 		offset += 4;
+
+		PBDisplayFixedint d;
+		d.display(tree, sub_tvb, id);
 	}
 
 	void dissect_varint(int id)
 	{
 		gint original_offset = offset;
-		gint val = parse_varint();
+		parse_varint(tvb, move(offset));
 
-		proto_tree_add_bytes_format(tree, protobuf_varint, tvb,
-			original_offset, offset - original_offset, NULL,
-			"unknown field %d: %d", id, val);
+		tvbuff_t *sub_tvb = tvb_new_subset_length(tvb,
+			original_offset, offset - original_offset);
+
+		PBDisplayVarint d;
+		d.display(tree, sub_tvb, id);
 	}
 
 	void dissect_str(int id)
 	{
-		guint8 length = parse_varint();
+		guint8 length = parse_varint(tvb, move(offset));
 
 		DISSECTOR_ASSERT(tvb_length_remaining(tvb, offset) >= length);
 
-		proto_tree_add_bytes_format(tree, protobuf_blob, tvb, offset, length, NULL,
-			"unknown field %d: string[%d]", id, length);
-
+		tvbuff_t *sub_tvb = tvb_new_subset_length(tvb, offset, length);
 		offset += length;
+
+		PBDisplayBlob d;
+		d.display(tree, sub_tvb, id);
 	}
 
 	void dissect_more()
 	{
-		gint64 header = parse_varint();
+		gint64 header = parse_varint(tvb, move(offset));
 		guint8 type = header & 7;
 		gint id = header >> 3;
 
--- dissect-protobuf.hpp
+++ dissect-protobuf.hpp
@@ -2,4 +2,40 @@ extern "C" {
 #include <epan/packet.h>
 }
 
+class PBDisplay
+{
+private:
+	int expected_type;
+public:
+	PBDisplay(int expected_type);
+
+	virtual void display(proto_tree *tree, tvbuff_t *tvb, int id, guint8 type);
+	virtual void display(proto_tree *tree, tvbuff_t *tvb, int id);
+	virtual void display(proto_tree *tree, tvbuff_t *tvb);
+};
+
+class PBDisplayFixedint : PBDisplay
+{
+public:
+	PBDisplayFixedint();
+
+	virtual void display(proto_tree *tree, tvbuff_t *tvb, int id);
+};
+
+class PBDisplayVarint : PBDisplay
+{
+public:
+	PBDisplayVarint();
+
+	virtual void display(proto_tree *tree, tvbuff_t *tvb, int id);
+};
+
+class PBDisplayBlob : PBDisplay
+{
+public:
+	PBDisplayBlob();
+
+	virtual void display(proto_tree *tree, tvbuff_t *tvb, int id);
+};
+
 void dissect_protobuf(tvbuff_t *tvb, proto_tree *tree);
